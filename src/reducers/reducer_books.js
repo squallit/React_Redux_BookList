eac68e2de1b238affcd099ad7a9231c556be0545
Paul Luu <paul.son.luu@gmail.com>
2018-09-24 22:36:49 -0700

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: "How To Manage Your Money"},
+    { title: "Finance 101: Money Matters"},
+    { title: "Our Nature In Our Hands"},
+    { title: "Object Oriented Programming With C++"},
+    { title: "Python And Django Full Stack BootCamp"}
+  ]
+}
